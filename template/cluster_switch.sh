ad42e426049eb90772fa820192bc7776e33ba3bc
Alexander Hägele <alexanderhagele@gmail.com>
2024-07-15 14:07:45 +0200

    update setups for 3 differnet clusters

---
+++ template/cluster_switch.sh
@@ -0,0 +1,22 @@
+function ic-cluster {
+    alias runai=runai-ic
+    # This is actually changing the context not the cluster ...
+    runai config cluster ic-caas
+    runai config project mlo-$GASPAR_USERNAME
+}
+function rcp-cluster {
+    alias runai=runai-rcp
+    # This is actually changing the context not the cluster ...
+    runai config cluster rcp-caas-test
+    runai config project mlo-$GASPAR_USERNAME
+}
+
+function rcp-prod-cluster {
+    alias runai=runai-rcp-prod
+    # This is actually changing the context not the cluster ...
+    runai config cluster rcp-caas-prod
+    runai config project mlo-$GASPAR_USERNAME
+}
+
+# We make RCP our default cluster.
+rcp-cluster
