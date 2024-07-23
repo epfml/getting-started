c2ca749e50a996be1cd410647d7ab38c804efcca
EduardDurech <39579228+EduardDurech@users.noreply.github.com>
2024-07-23 20:31:26 +0200

    Set HOME to working_dir

--- csub.py
+++ csub.py
@@ -199,7 +199,7 @@ spec:
   environment:
     items:
       HOME:
-        value: "/home/{user_cfg['user']}"
+        value: "{working_dir}"
       NB_USER:
         value: {user_cfg['user']}
       NB_UID:
